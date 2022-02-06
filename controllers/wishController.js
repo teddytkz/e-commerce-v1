f71d742c158e26a2f546da5a883534ec93a257ed
teddytkz <tkzteddy@gmail.com>
2022-02-06 12:42:46 +0700

    update Wish, add function get wish and remove wish

--- controllers/wishController.js
+++ controllers/wishController.js
@@ -1,5 +1,7 @@
 const Wish = require('../models/wishModel')
 const Product = require('../models/productsModel')
+const Brand = require('../models/brandModel')
+const Type = require('../models/typeModel')
 
 
 exports.addWish = async function (req, res) {
@@ -28,15 +30,66 @@ exports.addWish = async function (req, res) {
         })
     } catch (err) {
         console.log(err)
-        res.status(400).json('Add Wish Failed')
+        res.status(400).json({ msg: 'Add Wish Failed' })
     }
 
 }
 
 exports.getWish = async function (req, res) {
-    res.json(req.user.userId)
+    const id_user = req.user.userId
+    try {
+        const wishs = await Wish.findAll({
+            include: [{
+                model: Product,
+                include: [{
+                    model: Brand
+                }, {
+                    model: Type
+                }]
+            }],
+            where: {
+                id_user: id_user
+            }
+        })
+        res.json(wishs)
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: 'Get Wish Failed' })
+    }
 }
 
 exports.removeWish = async function (req, res) {
-
+    const { id_wish } = req.body
+    try {
+        const wishs = await Wish.findOne({
+            where: {
+                id_wish: id_wish
+            }
+        }).then(async (result) => {
+            const products = await Product.findOne({
+                attributes: ['product_wish']
+            }, {
+                where: {
+                    id_product: result.id_product
+                }
+            })
+            const updateProductWish = await Product.update({
+                product_wish: parseInt(products.product_wish) - 1
+            }, {
+                where: {
+                    id_product: result.id_product
+                }
+            })
+        }).then(() => {
+            const wishs = Wish.destroy({
+                where: {
+                    id_wish: id_wish
+                }
+            })
+            res.json({ msg: "Remove Wish Success" })
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: 'Remove Wish Failed' })
+    }
 }
