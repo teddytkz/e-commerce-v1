894ae3ab89e86f863253642f40fd449085028133
teddytkz <tkzteddy@gmail.com>
2022-02-06 12:16:07 +0700

    add Product Router for Update by Id and add product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController')
+const productImages = require('../models/productImagesModel')
 
 //GET
 router.get('/', productController.getAllProduct)
 router.get('/:id', productController.getProduct)
+
+//PUT
+router.put('/:id', productController.updateProduct)
+
+//POST
+router.post('/addProduct', productController.upload.array('images', 10), productController.createProduct)
 
 module.exports = router
